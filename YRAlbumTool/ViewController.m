e416025d0c57ec7cba7c5bf0c78ff04e3a99b1a9
yangrui <yangrui@yangruidemac-mini.local>
2017-11-22 19:05:03 +0800

    Initial Commit

---
+++ YRAlbumTool/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YRAlbumTool
+//
+//  Created by 　yangrui on 2017/11/22.
+//  Copyright © 2017年 　yangrui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
